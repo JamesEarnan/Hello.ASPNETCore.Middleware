f027ff3bdb89d01294d33de8396b85a6d9950d4e
Villaverde, James Earnan Dayo <james.earnan06@gmail.com>
2018-06-09 14:41:38 +0800

    Add project files.

---
+++ Hello.ASPNETCore.Middleware/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hello.ASPNETCore.Middleware.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
